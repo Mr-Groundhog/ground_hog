55bfbe9dfeedafece0fe161a52fdc4adcaf9e17c
Mr-Groundhog <1304764264@qq.com>
2026-01-21 21:04:58 +0800

    perf(tools): 强制动态渲染以提高页面性能

--- src/app/(site)/tools/page.tsx
+++ src/app/(site)/tools/page.tsx
@@ -4,6 +4,8 @@ import { ToolboxClient } from "./components/toolbox-client";
 import { Skeleton } from "@/components/ui/skeleton";
 import { getDistinctToolCategories } from "@/app/dashboard/tools/actions";
 
+export const dynamic = "force-dynamic";
+
 export default async function ToolboxPage() {
   const tools = await getPublicTools();
   const categories = await getDistinctToolCategories();
